9e23107ad3edaf73f1c6abf2049fb76d79a23b70
Felipe <2497307+felipeluna@users.noreply.github.com>
2023-11-20 15:06:52 +0100

    work

--- keyboards/handwired/dactyl_manuform/4x6_5/keymaps/luna/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6_5/keymaps/luna/keymap.c
@@ -45,13 +45,13 @@ LCTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,  
         KC_LGUI, KC_Z,    KC_X,    KC_C,    MT_V,    KC_B,               KC_N,    MT_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RGUI,
                           MLALT,    MLCTL,                                                 MRCTL,    MRALT,
                                        LE_RAIS,  KC_SPC,   KC_TAB,   KC_ENT,   KC_BSPC,  LD_LOW,
-                                                MLSFT, KC_ESC,        MO(_SYM) ,  MRSFT
+                                                MLSFT, MO(_SYM),        MO(_SYM) ,  MRSFT
     ),
 
     [_LOWER] = LAYOUT(
        _______, KC_LBRC , KC_7    , KC_8    , KC_9    , KC_RBRC ,      KC_PGUP , KC_HOME   , KC_CAPS , KC_END , KC_PSCR ,  _______,
        _______, KC_SCLN , KC_4    , KC_5    , KC_6    , KC_EQL  ,      KC_LEFT , KC_DOWN   , KC_UP   , KC_RGHT, KC_INS  ,  _______,
-       _______, MT_GRV  , KC_1    , KC_2    , KC_3    , KC_BSLS ,      KC_PGDN , KC_LPRN   , KC_RPRN, KC_UNDS , KC_RGUI ,  _______,
+       _______, MT_GRV  , KC_1    , KC_2    , KC_3    , KC_BSLS ,      KC_PGDN , MRSFT   , KC_RPRN, KC_UNDS , KC_RGUI ,  _______,
                           _______, _______,                                                 _______, _______,
                                   _______,   KC_0  , KC_MINS,             _______, _______, _______,
                                              _______, KC_DOT,             _______, _______
@@ -59,8 +59,8 @@ LCTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,  
 
     [_RAISE] = LAYOUT(
         _______, KC_NO  , KC_F7  , KC_F8  , KC_F9 , KC_F12,            _______, _______, _______, _______,  _______, _______,
-        _______, KC_LEFT, KC_F4  , KC_F5  , KC_F6 , KC_F11,            _______, _______, _______, _______,  _______, _______,
-        _______, KC_LGUI, KC_F1  , KC_F2 , KC_F3 , KC_F10,            _______, _______, _______, _______,  _______, _______,
+        _______, KC_LEFT, KC_F4  , KC_F5  , KC_F6 , KC_F11,            _______, KC_RPRN, KC_RPRN, _______,  _______, _______,
+        _______, KC_LGUI, KC_F1  , KC_F2 , KC_F3 , KC_F10,             _______, _______, _______, _______,  _______, _______,
                           _______, _______,                                                _______, _______,
                                   _______, _______,  _______,            KC_TRNS , KC_QUES , _______,
                                             _______, _______,            _______ , _______
